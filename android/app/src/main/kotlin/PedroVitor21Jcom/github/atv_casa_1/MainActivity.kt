ec91e3563f571ea46695d9f784fa8ddd892c60d4
Pedro Vitor <pedrovtor8@gmail.com>
2025-09-04 15:09:27 -0300

    primeiro commit

---
+++ android/app/src/main/kotlin/PedroVitor21Jcom/github/atv_casa_1/MainActivity.kt
@@ -0,0 +1,5 @@
+package PedroVitor21Jcom.github.atv_casa_1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,40 @@
+/*Crie um aplicativo que faça 5 perguntas para uma pessoa sobre um crime.  
+As perguntas são: 
+• “Telefonou para a vítima?” 
+• “Esteve no local do crime?” 
+• “Mora perto da vítima?” 
+• “Tinha dívidas com a vítima?” 
+• “Já trabalhou com a vítima?” 
+O usuário deve responder apenas SIM ou NÃO. O programa deve no final emitir uma classificação sobre 
+a participação da pessoa no crime.  
+• Se a pessoa responder positivamente a 2 questões ela deve ser classificada como “Suspeita”;  
+• Entre 3 e 4 como “Cúmplice”;  
+• 5 questões positivas será considerado como “Assassino”.  
+• Caso contrário, ele será classificado como “Inocente”. 
+OBS: 
+• DEVERÁ ser utilizado o Provider como gerenciador de estados e a organização de pastas deve 
+atender ao Padrão de Arquitetura MVVM.*/
+
+import 'package:flutter/material.dart';
+import 'package:atv_casa_1/models/interrogatorio_models.dart';
+import 'package:atv_casa_1/views/interrogatorio_views.dart';
+import 'package:atv_casa_1/views_models/interrogatorio_views_models.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Text('Hello World!'),
+        ),
+      ),
+    );
+  }
+}
